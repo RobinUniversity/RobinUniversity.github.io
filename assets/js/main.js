5029feea56b39730a39a7d36419fa9f901becc80
Robin Cunningham <rjcuniversity@yahoo.com>
2023-03-29 16:36:52 +0000

    added project files

---
+++ assets/js/main.js
@@ -0,0 +1,431 @@
+(function($) {
+
+	var	$window = $(window),
+		$body = $('body'),
+		$wrapper = $('#wrapper'),
+		$header = $('#header'),
+		$banner = $('#banner');
+
+	// Breakpoints.
+		breakpoints({
+			xlarge:    ['1281px',   '1680px'   ],
+			large:     ['981px',    '1280px'   ],
+			medium:    ['737px',    '980px'    ],
+			small:     ['481px',    '736px'    ],
+			xsmall:    ['361px',    '480px'    ],
+			xxsmall:   [null,       '360px'    ]
+		});
+
+	/**
+	 * Applies parallax scrolling to an element's background image.
+	 * @return {jQuery} jQuery object.
+	 */
+	$.fn._parallax = (browser.name == 'ie' || browser.name == 'edge' || browser.mobile) ? function() { return $(this) } : function(intensity) {
+
+		var	$window = $(window),
+			$this = $(this);
+
+		if (this.length == 0 || intensity === 0)
+			return $this;
+
+		if (this.length > 1) {
+
+			for (var i=0; i < this.length; i++)
+				$(this[i])._parallax(intensity);
+
+			return $this;
+
+		}
+
+		if (!intensity)
+			intensity = 0.25;
+
+		$this.each(function() {
+
+			var $t = $(this),
+				on, off;
+
+			on = function() {
+
+				$t.css('background-position', 'center 100%, center 100%, center 0px');
+
+				$window
+					.on('scroll._parallax', function() {
+
+						var pos = parseInt($window.scrollTop()) - parseInt($t.position().top);
+
+						$t.css('background-position', 'center ' + (pos * (-1 * intensity)) + 'px');
+
+					});
+
+			};
+
+			off = function() {
+
+				$t
+					.css('background-position', '');
+
+				$window
+					.off('scroll._parallax');
+
+			};
+
+			breakpoints.on('<=medium', off);
+			breakpoints.on('>medium', on);
+
+		});
+
+		$window
+			.off('load._parallax resize._parallax')
+			.on('load._parallax resize._parallax', function() {
+				$window.trigger('scroll');
+			});
+
+		return $(this);
+
+	};
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+	// Clear transitioning state on unload/hide.
+		$window.on('unload pagehide', function() {
+			window.setTimeout(function() {
+				$('.is-transitioning').removeClass('is-transitioning');
+			}, 250);
+		});
+
+	// Fix: Enable IE-only tweaks.
+		if (browser.name == 'ie' || browser.name == 'edge')
+			$body.addClass('is-ie');
+
+	// Scrolly.
+		$('.scrolly').scrolly({
+			offset: function() {
+				return $header.height() - 2;
+			}
+		});
+
+	// Tiles.
+		var $tiles = $('.tiles > article');
+
+		$tiles.each(function() {
+
+			var $this = $(this),
+				$image = $this.find('.image'), $img = $image.find('img'),
+				$link = $this.find('.link'),
+				x;
+
+			// Image.
+
+				// Set image.
+					$this.css('background-image', 'url(' + $img.attr('src') + ')');
+
+				// Set position.
+					if (x = $img.data('position'))
+						$image.css('background-position', x);
+
+				// Hide original.
+					$image.hide();
+
+			// Link.
+				if ($link.length > 0) {
+
+					$x = $link.clone()
+						.text('')
+						.addClass('primary')
+						.appendTo($this);
+
+					$link = $link.add($x);
+
+					$link.on('click', function(event) {
+
+						var href = $link.attr('href');
+
+						// Prevent default.
+							event.stopPropagation();
+							event.preventDefault();
+
+						// Target blank?
+							if ($link.attr('target') == '_blank') {
+
+								// Open in new tab.
+									window.open(href);
+
+							}
+
+						// Otherwise ...
+							else {
+
+								// Start transitioning.
+									$this.addClass('is-transitioning');
+									$wrapper.addClass('is-transitioning');
+
+								// Redirect.
+									window.setTimeout(function() {
+										location.href = href;
+									}, 500);
+
+							}
+
+					});
+
+				}
+
+		});
+
+	// Header.
+		if ($banner.length > 0
+		&&	$header.hasClass('alt')) {
+
+			$window.on('resize', function() {
+				$window.trigger('scroll');
+			});
+
+			$window.on('load', function() {
+
+				$banner.scrollex({
+					bottom:		$header.height() + 10,
+					terminate:	function() { $header.removeClass('alt'); },
+					enter:		function() { $header.addClass('alt'); },
+					leave:		function() { $header.removeClass('alt'); $header.addClass('reveal'); }
+				});
+
+				window.setTimeout(function() {
+					$window.triggerHandler('scroll');
+				}, 100);
+
+			});
+
+		}
+
+	// Banner.
+		$banner.each(function() {
+
+			var $this = $(this),
+				$image = $this.find('.image'), $img = $image.find('img');
+
+			// Parallax.
+				$this._parallax(0.275);
+
+			// Image.
+				if ($image.length > 0) {
+
+					// Set image.
+						$this.css('background-image', 'url(' + $img.attr('src') + ')');
+
+					// Hide original.
+						$image.hide();
+
+				}
+
+		});
+
+	// Menu.
+		var $menu = $('#menu'),
+			$menuInner;
+
+		$menu.wrapInner('<div class="inner"></div>');
+		$menuInner = $menu.children('.inner');
+		$menu._locked = false;
+
+		$menu._lock = function() {
+
+			if ($menu._locked)
+				return false;
+
+			$menu._locked = true;
+
+			window.setTimeout(function() {
+				$menu._locked = false;
+			}, 350);
+
+			return true;
+
+		};
+
+		$menu._show = function() {
+
+			if ($menu._lock())
+				$body.addClass('is-menu-visible');
+
+		};
+
+		$menu._hide = function() {
+
+			if ($menu._lock())
+				$body.removeClass('is-menu-visible');
+
+		};
+
+		$menu._toggle = function() {
+
+			if ($menu._lock())
+				$body.toggleClass('is-menu-visible');
+
+		};
+
+		$menuInner
+			.on('click', function(event) {
+				event.stopPropagation();
+			})
+			.on('click', 'a', function(event) {
+
+				var href = $(this).attr('href');
+
+				event.preventDefault();
+				event.stopPropagation();
+
+				// Hide.
+					$menu._hide();
+
+				// Redirect.
+					window.setTimeout(function() {
+						window.location.href = href;
+					}, 250);
+
+			});
+
+		$menu
+			.appendTo($body)
+			.on('click', function(event) {
+
+				event.stopPropagation();
+				event.preventDefault();
+
+				$body.removeClass('is-menu-visible');
+
+			})
+			.append('<a class="close" href="#menu">Close</a>');
+
+		$body
+			.on('click', 'a[href="#menu"]', function(event) {
+
+				event.stopPropagation();
+				event.preventDefault();
+
+				// Toggle.
+					$menu._toggle();
+
+			})
+			.on('click', function(event) {
+
+				// Hide.
+					$menu._hide();
+
+			})
+			.on('keydown', function(event) {
+
+				// Hide on escape.
+					if (event.keyCode == 27)
+						$menu._hide();
+
+			});
+
+			var filtersList = {};
+
+filterSelection(document.getElementsByName("myBtnContainer")[0].children[0]);
+
+function renameAll(i) {
+  return i.replace('Show all', 'all');
+}
+function filterSelection(current) {
+  var x, i;
+  var c = renameAll(current.innerText.trim());
+  var buttons = document.getElementsByName("myBtnContainer");
+  var group = current;
+  do {
+    group = group.parentElement;
+    if (group.getAttribute("name") == "myBtnContainer") {
+      for (i = 0;i<buttons.length;i++) {
+        if (buttons[i] === group) {
+          group = i + 1;
+          break;
+        }
+      }
+      break;
+    }
+  } while (true);
+  var has = filtersList[c];
+  if (!has) {
+    if (filtersList.all && group == 1) filtersList = {};
+    if (c == 'all') filtersList = { all:1 }
+    else filtersList[c] = group;
+  } else {
+    delete filtersList[c];
+    if (c == 'all') filtersList = {};
+  }
+  var records = document.getElementsByClassName("column");
+  var matched = 0, filtersNo = [null,null];
+  updateButtons(buttons, filtersNo);
+  for (i = 0; i < records.length; i++) {
+    w3RemoveClass(records[i], "show");
+    var matchedFilters = [0,0];
+    for(var c1 in filtersList) {
+      var group = filtersList[c1]-1;
+      if (c1 == "all") c1 = "";
+      if (records[i].className.indexOf(c1) > -1) {
+        matchedFilters[group]++;
+  }
+    }
+    // Display trick here 2 binary numbers or logic table - each bit means non-zero number in group (matched > 0 / selected filter > 0) - hope works properly ;-)
+    var matched = (!!matchedFilters[0]) + (!!matchedFilters[1])*2;
+    var filters = (!!filtersNo[0]) + (!!filtersNo[1])*2;
+    if ((matched & filters) == filters && (filters != 0)) {
+          w3AddClass(records[i], "show");
+    }
+  }
+  updateButtons(buttons, filtersNo);
+}
+
+function updateButtons(buttons, filtersNo) {
+  filtersNo[0] = 0;
+  filtersNo[1] = 0;
+  for (i = 0;i<buttons.length;i++) {
+    if (i && !filtersNo[0]) filtersList = {};
+    for (var j = 0;j<buttons[i].children.length;j++) {
+      var el = buttons[i].children[j];
+      c = renameAll(el.innerHTML.trim());
+      if (filtersList[c]) {
+        filtersNo[i]++;
+        if (el.className.indexOf("active") < 0) el.className += " active";
+      } else {
+        el.className = el.className.replace(/[ ]*active[ ]*/,'');
+      }
+    }
+  }
+  if (filtersNo[0]) {
+    buttons[1].style.display = "";
+  } else {
+    buttons[1].style.display = "none";
+  }
+}
+
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+})(jQuery);
